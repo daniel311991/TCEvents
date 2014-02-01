c9ebc7c60a836fa32df0de510b7905625f63fe96
Daniel <daniel.iskandar31@gmail.com>
2014-02-01 18:00:12 +1000

    returned jumbotron2 box special page

--- special.php
+++ special.php
@@ -159,6 +159,47 @@
         </div> <!-- /.container-->
     </div> <!-- /.jumbotron-->
 
+    <div class="jumbotron2 blogfeed bg_<?php echo $bg_color ?>">
+        <div class="container">
+
+            <h1 class="font-bebas2">WHAT WE CAN DO TOGETHER</h1>
+            
+            <div class="jumbo_content">
+               <div class="first-third col col-lg-4">
+                <h2>First Section</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div>
+
+            <div class="second-third col col-lg-4">
+                <h2>Second Section</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div>
+
+            <div class="third-third col col-lg-4">
+                <h2>Third Section</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            </div>
+            </div>
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
 
     <div class="jumbotron2 startsection bg_white">
         <div class="container">
