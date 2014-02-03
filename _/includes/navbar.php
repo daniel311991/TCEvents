5654ac29ecdf35435c189a077e6cd17616b561f8
Hans Barroga <hans.barroga@gmail.com>
2014-02-04 00:43:54 +1000

    Added hover colours for each nav link

--- _/includes/navbar.php
+++ _/includes/navbar.php
@@ -17,13 +17,13 @@
                 </button> <!-- /.navbar-toggle -->
 
                 <div class="collapse navbar-collapse navHeaderCollapse">
-                    <ul class="nav navbar-nav navbar-right '.$bg_color.'-hover">
-                        <li '.$toursactive.'><a href="tours.php"><i class="fa fa-sun-o fa-fw"></i> Tours</a></li>
-                        <li '.$corpactive.'><a href="corporate.php"><i class="fa fa-suitcase fa-fw"></i> Corporate Tours</a></li>
-                        <li '.$specialactive.'><a href="special.php"><i class="fa fa-glass fa-fw"></i> Special Events</a></li>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li '.$toursactive.' ><a class="lightblue-hover" href="tours.php"><i class="fa fa-sun-o fa-fw"></i> Tours</a></li>
+                        <li '.$corpactive.' ><a class="dark_blue-hover" href="corporate.php"><i class="fa fa-suitcase fa-fw"></i> Corporate Tours</a></li>
+                        <li '.$specialactive.' ><a class="pink-hover" href="special.php"><i class="fa fa-glass fa-fw"></i> Special Events</a></li>
                         <li><a href="#"><i class="fa fa-book fa-fw"></i> Blog</a></li>
-                        <li '.$mystoryactive.' ><a href="mystory.php"><i class="fa fa-quote-right fa-fw"></i> My Story</a></li>
-                        <li '.$contactactive.'><a href="contact.php"><i class="fa fa-phone fa-fw"></i> Contact Us</a></li>
+                        <li '.$mystoryactive.' ><a class="orange-hover" href="mystory.php"><i class="fa fa-quote-right fa-fw"></i> My Story</a></li>
+                        <li '.$contactactive.' ><a class="purple-hover" href="contact.php"><i class="fa fa-phone fa-fw"></i> Contact Us</a></li>
                     </ul> <!-- /.navbar-nav -->
                 </div> <!-- /.navHeaderCollapse -->    
             </div> <!-- /.container -->   
