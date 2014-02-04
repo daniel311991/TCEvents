b7f0993dc81101cd59e47b3f4977a53e3a685038
Daniel <daniel.iskandar31@gmail.com>
2014-02-04 22:51:34 +1000

    Fixed up css for aboutMe page

--- mystory.php
+++ mystory.php
@@ -43,111 +43,335 @@
         </div> <!-- /.container-->
     </div> <!-- /.jumbotron-->
 
-    <div class="jumbotron pic_jumbo bg_<?php echo $bg_color ?>">
-        <div class="container">
+    <div class="jumbotron pic_jumbo">
+        
             <div class="row">
-               
-                 <!--<div class="jumbo_content col-lg-8 col-lg-offset-2">
-                    <p class="big_p"> We&rsquo;re here because we <em>love</em> events and we do them well. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                </div> -->
-                <div class="first-third col col-lg-4 pic_jumbo">
+            
+                <div class="first-third col col-lg-4 ">
                         
                     </div> <!-- first-third -->
                         
-                     <div class="second-third col col-lg-4 pic_jumbo">
+                     <div class="second-third col col-lg-4 ">
                        
                     </div> <!-- second-third -->
-                     <div class="third-third col col-lg-4 pic_jumbo">
+                     <div class="third-third col col-lg-4 ">
                        
                     </div> <!-- /.third-third -->
             </div>
-        </div> <!-- /.container -->
+        <!-- /.container -->
     </div> <!-- /.jumbotron-->
 
-    <div class="jumbotron bg_white">
+    <div class="jumbotron bg_white text_jumbo">
         <div class="container">
 
-            <h1 class="font-bebas2">BLOG FEED</h1>
-            
-            <div class="jumbo_content">
-               <div class="first-third col col-lg-4">
-                <h2>First Section</h2>
-                <p class="small_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-
-            <div class="second-third col col-lg-4">
-                <h2>Second Section</h2>
-                <p class="small_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-
-            <div class="third-third col col-lg-4">
-                <h2>Third Section</h2>
-                <p class="small_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-            </div>
+            <p>
+                Events surrounded my life! I remember running around in weddings, not to mention 
+                the fact that my parents have always hosted an annual St George’s Feast in our home and we 
+                often had a turnaround of 200+ guests, every year. It was always fun, I enjoyed inviting
+                my family, friend & strangers to celebrate the feast. I spend the lead up time preparing and 
+                talking about St George and why we celebrated the feast and it’s a story that never gets old.
+                And weddings, well I have been in so many, not as a guest, mostly as part of the wedding party, 
+                weddings followed me into the different stages of my life. I was a flower girl a few times, 
+                I dragged the dress a few other times and a bridesmaid more than I should! The Chinese would 
+                call it a curse, but I call it a duty to make someone happy on a day they will always remember.
+            </p>    
+           
         </div> <!-- /.container-->
     </div> <!-- /.jumbotron-->
 
-    <div class="jumbotron bg_<?php echo $bg_color ?>">
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo1">
         <div class="container">
 
-            <h1 class="font-bebas2">WHAT WE CAN DO TOGETHER</h1>
-            
-            <div class="jumbo_content">
-               <div class="first-third col col-lg-4">
-                <h2>First Section</h2>
-                <p class="small_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-
-            <div class="second-third col col-lg-4">
-                <h2>Second Section</h2>
-                <p class="small_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-
-            <div class="third-third col col-lg-4">
-                <h2>Third Section</h2>
-                <p class="small_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            </div>
-            </div>
+            <p>
+               The only thing I never liked is the camera, but I guess that’s part of the job. 
+               If you are part of the bridal party, you are followed by the camera. The best 
+               thing about been an events director is that you are invisible to the lens.
+            </p>    
+           
         </div> <!-- /.container-->
     </div> <!-- /.jumbotron-->
+
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo2">
+        <div class="container">
+
+            <p>
+                I have dreamed about opening my own events and tourism business since 2006. I’ve built my connections 
+                in the industry and it’s a grand, competitive one. Finally I am here and ready. I have walked the 
+                layouts and designs of weddings from all walks of life. From an Australian wedding, an Asian wedding, 
+                a Muslim Arab wedding, an Indian Wedding, an Egyptian wedding, an Eritrean wedding, to an East African
+                wedding and I can assure you while they are all celebrate the union of two souls, it’s important to be 
+                aware of the culture involve, sometimes it’s a fusion of many cultures. 
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo">
+        <div class="container">
+
+            <p>
+                All this different type of weddings have one thing in common, confused brides who appreciated 
+                the direction. Most brides have a vision of what their wedding day should look like but have 
+                no idea how to get there or the cost involved in getting there, not only the dollar value but 
+                the emotional and physical investment. They also don’t have the cash to spend on a planner to 
+                take care of the wedding from A-z. What I have to offer is something called ‘wedding consulting’,
+                which means we help you plan it, we save you time and money, you execute the plan yourself, but
+                we have check-points where we ensure you are ok. We also have wedding directors who are happy
+                to help you on the actual day of your wedding, because a bride can’t run the show and be the 
+                show at the same time. The wedding directors look at your plan and bring it to life in the big
+                day. We of course have the exclusive offer of taking care of the whole events from planning to
+                executing. 
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo3">
+        <div class="container">
+
+            <p>
+                Events don’t only mean weddings and engagements, that’s why I divide them
+                into three different events, there are the heart to heart events, the corporate
+                and the tourism events, where people are making memories that belong to a theme 
+                and Queensland. 
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo3">
+        <div class="container">
+
+            <p>
+               I have been to cities where I was hiding inside buildings working from the sunrise till the sun
+                sets and I would fly back home and talk about how much I hated the city. The truth is, I didn’t 
+                know the city, what I hated is residing in the same building wishing my life away. I realised 
+                that bitter reality, when I decided to take a day and visit the city ‘outside’ the office.
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo2">
+        <div class="container">
+
+            <p>
+                I saw it and it healed my heart that bled every time I landed in that city. Beside the buildings, 
+                the traffic and the people who never smiled, there were people outside who smiled from ear to ear, 
+                beautiful buildings that invited you to come-in and trams that roomed places I didn’t know existed. 
+                Seen the city, changed my mood and transformed my perspective, I was actually motivated to work there
+                and I was far from counting the hours till my next flight. I have seen it from a local’s eye. 
+
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo1">
+        <div class="container">
+
+            <p>
+                That city wasn’t Brisbane, but I see colleagues who had boxed Brisbane into the boring work 
+                city, that steals their soul and I want to tell them to come with me, you can trust me, I 
+                have been to 26 cities in 4 different continents. 
+
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo3">
+        <div class="container">
+
+            <p>
+                You can say, I love traveling!  It’s not the travel I am drawn to; I am naturally a more grounded person,
+                but knowledge has always been my weakness. The experience of intertwining with  a different wind in a 
+                different place that whispers a different story is what chase. 
+
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron pic_jumbo">
+        
+            <div class="row">
+            
+                <div class="first-third col col-lg-4 ">
+                        
+                    </div> <!-- first-third -->
+                        
+                     <div class="second-third col col-lg-4 ">
+                       
+                    </div> <!-- second-third -->
+                     <div class="third-third col col-lg-4 ">
+                       
+                    </div> <!-- /.third-third -->
+            </div>
+        <!-- /.container -->
+    </div> <!-- /.jumbotron-->
+
+    <div class="jumbotron bg_white text_jumbo2">
+        <div class="container">
+
+            <p>
+                I would like to visit each city again but I never did. I have however always landed back home,
+                I always came back to claim the piece of me that I left behind. Home, Brisbane, has always been
+                the most beautiful place I have ever seen, and not to be bias but even the Sun shines brighter here. 
+                This city has courted many hidden tales and yet pure and uncorrupted of the commercial world it remains.
+                When you see it, I want you to take a peek through the eye of a local and truly appreciate a tour with us. 
+
+            </p>    
+           
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+   
 
     <div class="jumbotron startsection bg_white">
         <div class="container">
