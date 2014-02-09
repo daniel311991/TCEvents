27f22ac274b651023e62aeaacd33a79e9dadefcc
Hans Barroga <hans.barroga@gmail.com>
2014-02-09 12:44:52 +1000

    Creating a coming soon page for eventstc.com.au

---
+++ comingsoon.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time Capsule Events | Coming Soon</title>
+
+    <link rel="stylesheet" href="_/css/comingsoon.css">
+    <link rel="stylesheet" href="_/css/tcstyles.css">
+    <link rel="stylesheet" href="_/css/bootstrap.css">
+
+    <!-- FontAweseome -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+</head>
+<body id="coming_soon">
+
+<!-- PHP navbar include -->
+    <?php
+        $specialactive = 'class = "speciallink active"';
+        $bg_color = 'pink';
+        $svgpath = '_/svg/tc_logo_'.$bg_color.'.svg';
+    ?>
+
+   
+
+        <div class="container">
+
+            <div class="jumbo_title">
+                <h1 class="font-bebas big_timeless"><span class="<?php echo $bg_color ?>">TIMECAPSULE EVENTS</span></h1>
+
+                <img src="<?php echo $svgpath ?>" class="tcnyou" alt="Responsive image" />
+                <h1 class="font-bebas">& YOU </h1>
+
+                <h1 class="font-bebas">COMING SOON</h1>
+            </div>
+        </div> <!-- /.container-->
+    </div> <!-- /.jumbotron-->
+
+     <!-- scripts -->
+        <script src="_/js/bootstrap.js"></script>
+        <script src="_/js/tcscripts.js"></script>
+    </body>
+
+    </html>
